36914c3be28576c74f46ba3d85636df4820bbbdc
Janiv <y1a2n3i4v55@gmail.com>
2022-06-14 12:58:20 +0300

    Home Products.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,11 +1,13 @@
 ( function () {
 
-    fetch('/api/top5product')
+
+    fetch('/api/top5products')
         .then(status)
         .then(json)
         .then(res=>{
+            document.getElementById("TopSalesList").innerHTML = ""
             let item_list = document.getElementById("TopSalesList")
-            for(let product in res.product){
+            for(let product of res){
                 addItemsList(product, item_list)
             }
     })
@@ -14,8 +16,9 @@
         .then(status)
         .then(json)
         .then(res=>{
+            document.getElementById("Products").innerHTML = ""
             let item_list = document.getElementById("Products")
-            for(let product in res.product){
+            for(let product of res){
                 addItemsList(product, item_list)
             }
         })
@@ -27,39 +30,39 @@
         let id = product.id;
         let listItem = document.createElement('div')
         listItem.setAttribute("id", `${product.id}`)
-        listItem.setAttribute("class", "col-3")
+        listItem.setAttribute("class", "border mt-2 col-md-2")
 
         //Need to add quantity if sold out
         listItem.innerHTML += `
-                                <h2>${product.name}</h2> <br/>
+                                <h3>${product.name}</h3> <br/>
                                 <img class="w-25" src="${product.image}"></img> <br/>
-                                Price: ${product.price} Price After Discount: ${product.price * (1 - ( product.discount / 100 ))}<br/>
+                                Price: ${product.price} Price After Discount: ${(product.price * (1 - ( product.discount / 100 ))).toPrecision(2)}<br/>
                                 <p id="quantity${id}">
                                     Quantity: ${product.counter}      
                                 </p>        
                               `
         let plusButton = document.createElement('button')
         plusButton.innerHTML = '+'
-        plusButton.addEventListener("click", function addQ(event) {
-            fetch(`/addToCart${id}`, {method: "PUT"})
-                .then(status)
-                .then(json)
-                .then()
-                .catch((err) => {
-                    console.log(err)
-                })
-        })
+        // plusButton.addEventListener("click", function addQ(event) {
+        //     fetch(`/addToCart${id}`, {method: "PUT"})
+        //         .then(status)
+        //         .then(json)
+        //         .then()
+        //         .catch((err) => {
+        //             console.log(err)
+        //         })
+        // })
         let minusButton = document.createElement('button')
         minusButton.innerHTML = '-'
-        minusButton.addEventListener("click", function minusQ(event){
-            fetch(`/minusToCart${id}`, {method: "PUT"})
-                .then(getStatusCode)
-                .then(json)
-                .then()
-                .catch((err)=>{
-                    console.log(err)
-                })
-        })
+        // minusButton.addEventListener("click", function minusQ(event){
+        //     fetch(`/minusToCart${id}`, {method: "PUT"})
+        //         .then(getStatusCode)
+        //         .then(json)
+        //         .then()
+        //         .catch((err)=>{
+        //             console.log(err)
+        //         })
+        // })
         listItem.append(plusButton);
         listItem.append(minusButton);
         item_list.append(listItem);
